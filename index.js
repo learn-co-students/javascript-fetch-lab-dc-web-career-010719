1c04d28b9d6e6aee0ca4429a4dd2f76bc0f18e40
James Clement <jameseclement@gmail.com>
2019-02-22 09:03:54 -0500

    done

--- index.js
+++ index.js
@@ -1,26 +1,78 @@
 const baseURL = 'https://api.github.com'
 
 function getIssues() {
+  const url = `${baseURL}/repos/learn-co-curriculum/javascript-fetch-lab/issues`
+  fetch(url,{
+    headers: {
+      Authorization: `token ${getToken()}`
+  }
+}).then(res=> res.json())
+  .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+let issueList = document.getElementById("issues")
+for (i=0; i<json.length; i++){
+
+  issueList.innerHTML += `<li>Title: <a href=${json[i].url}>${json[i].title} </a><span> | Body: ${json[i].body}</span></li>`
+}
 }
 
 function createIssue() {
+
+  let issueTitle = document.getElementById("title").value
+  let issueContent = document.getElementById("body").value
+  const postData = {
+    title: issueTitle,
+    body: issueContent
+  }
+
   console.log("Inside createIssue function")
+  const url = `${baseURL}/repos/learn-co-curriculum/javascript-fetch-lab/issues`
+  fetch(url, {
+    method:"POST",
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+  }
+  })
+
+  .then(getIssues())
 }
 
-function showResults(json) {
+
+
+
+
+function makeRepoLinkDiv(url){
+  let childAnnouncement = document.createElement('div')
+    childAnnouncement.innerHTML =  `<h3>Forked Successfully!</h3><a href='${url}'>Go To Fork</a>`
+    showResults(childAnnouncement)
+}
+
+let resultsDiv = document.getElementById("results")
+
+function showResults(child) {
+
+resultsDiv.appendChild(child)
 }
 
 function forkRepo() {
   console.log("Inside forkRepo function")
   const url = `${baseURL}/repos/learn-co-curriculum/javascript-fetch-lab/forks`
-  //use fetch to fork it!
+    fetch(url, {
+    method: 'POST',
+    headers: {
+    Authorization: `token ${getToken()}`
+  }
+})
+.then(res=> res.json())
+  .then(json => makeRepoLinkDiv(json.html_url))
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+
+  return '995457aa5b98a47d1793f939195c8da6334d0330'
 }
